8d338b1695639b2b337a1666588973c55a79b0b3
Bharathi1603 <bharathikec013@gmail.com>
2024-07-10 13:58:13 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,7 +9,7 @@
 class StringCalculator
 {
 public:
-      int add(const std::string input);
+      int add(std::string input);
 private:
       void findAndReplaceCharacter(std::string& str, char findCharacter, char replaceCharacter);
 
