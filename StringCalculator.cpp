f22648152d1ff7317f12019390296cd7b106b3b1
Bharathi1603 <bharathikec013@gmail.com>
2024-07-10 13:11:10 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -10,6 +10,8 @@ int StringCalculator::add(const std::string input)
   {
     char delimiter = ',';
     findAndReplaceCharacter(input,'\n', delimiter);
+    auto numbers = splitAndFilterNumbers(input,delimiter);
+    return performAddition(numbers);
   }
   return -1;
 }
@@ -23,3 +25,48 @@ void StringCalculator::findAndReplaceCharacter(std::string& str, char findCharac
         }
     } 
 }
+
+  std::vector<int> StringCalculator::splitAndFilterNumbers(const std::string& str, char delimiter)
+  {
+      std::vector<int> numbers;
+      std::string token;
+      std::stringstream ss(str);
+  
+      while (std::getline(ss, token, delimiter)) 
+      {
+          std::string numberStr;
+          for (char ch : token) 
+          {
+              if(ch == '-')
+              {
+                  numberStr += ch;
+              }
+              
+              if (std::isdigit(ch)) 
+              {
+                  numberStr += ch;
+              }
+          }
+          
+          if (!numberStr.empty()) 
+          {
+              numbers.push_back(std::stoi(numberStr));
+          }
+      }
+  
+      return numbers;
+  }
+  
+  int StringCalculator::performAddition(std::vector<int> numbers)
+  {
+      int sum = 0;
+      for(auto& data : numbers)
+      {
+          if(data <= 1000)
+          {
+              sum += data;  
+          }
+      }
+      
+      return sum;
+  }
