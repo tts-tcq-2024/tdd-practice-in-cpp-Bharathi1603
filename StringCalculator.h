9819fef76d8b89e7da374cda354b45c77890b7a2
Bharathi1603 <bharathikec013@gmail.com>
2024-07-10 15:08:06 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -12,17 +12,14 @@ public:
       int add(std::string input);
 private:
       void findAndReplaceCharacter(std::string& str, char findCharacter, char replaceCharacter);
-
       std::vector<int> splitAndFilterNumbers(const std::string& str, char delimiter);
-
       int performAddition(std::vector<int> numbers);
-
       void appendCharacter(std::string & numberStr, const char ch);
-
       std::string spiltDelimiter(const std::string token, const char delimiter);
-
       std::vector<int> getNegativeValues(std::vector<int> numbers);
-
       void checkNegativeValues(const std::vector<int> negativeNumbers);
+      bool isChangeDelimiterPresent(std::string & str);
+      char captureNewDelimiter(std::string & str);
+      void splitChangeDelimiterInfo(std::string & str);
 
 };
