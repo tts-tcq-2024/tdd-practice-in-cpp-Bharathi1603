f8167e15dec7a0e6235c678e3744969b2d5748df
Bharathi1603 <bharathikec013@gmail.com>
2024-07-10 14:37:31 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -18,56 +18,61 @@ int StringCalculator::add(std::string input)
 
 void StringCalculator::findAndReplaceCharacter(std::string& str, char findCharacter, char replaceCharacter)
 {
-    for (size_t i = 0; i < str.length(); ++i) {
-        if (str[i] == findCharacter) 
-        {
-            str[i] = replaceCharacter;
-        }
-    } 
+  for (size_t i = 0; i < str.length(); ++i) 
+  {
+      if (str[i] == findCharacter) 
+      {
+          str[i] = replaceCharacter;
+      }
+  } 
 }
 
-std::string StringCalculator::spiltDelimiterFrom(const std::string token, const char delimiter)
+std::vector<int> StringCalculator::splitAndFilterNumbers(const std::string& str, char delimiter)
 {
-    std::string numberStr;
-    for (char ch : token) 
+    std::vector<int> numbers;
+    std::string token;
+    std::stringstream ss(str);
+
+    while (std::getline(ss, token, delimiter)) 
     {
-        if((ch == '-') || std::isdigit(ch))
+        std::string numberStr = spiltDelimiter(token, delimiter);
+        if (!numberStr.empty()) 
         {
-            numberStr += ch;
+            numbers.push_back(std::stoi(numberStr));
         }
     }
-    
-    return numberStr;
+
+    return numbers;
 }
 
-  std::vector<int> StringCalculator::splitAndFilterNumbers(const std::string& str, char delimiter)
+int StringCalculator::performAddition(std::vector<int> numbers)
+{
+    int sum = 0;
+    for(auto& data : numbers)
+    {
+        if(data <= 1000)
+        {
+            sum += data;  
+        }
+    }
+    return sum;
+}
+
+void StringCalculator::appendCharacter(std::string & numberStr, const char ch)
+{
+    if((ch == '-') || std::isdigit(ch))
+    {
+        numberStr += ch;
+    }
+}
+
+std::string StringCalculator::spiltDelimiter(const std::string token, const char delimiter)
+{
+  std::string numberStr;
+  for (char ch : token) 
   {
-      std::vector<int> numbers;
-      std::string token;
-      std::stringstream ss(str);
-  
-      while (std::getline(ss, token, delimiter)) 
-      {
-          std::string numberStr = spiltDelimiterFrom(token, delimiter);
-          if (!numberStr.empty()) 
-          {
-              numbers.push_back(std::stoi(numberStr));
-          }
-      }
-  
-      return numbers;
+      appendCharacter(numberStr, ch);
   }
   
-  int StringCalculator::performAddition(std::vector<int> numbers)
-  {
-      int sum = 0;
-      for(auto& data : numbers)
-      {
-          if(data <= 1000)
-          {
-              sum += data;  
-          }
-      }
-      
-      return sum;
-  }
+  return numberStr;
+}
