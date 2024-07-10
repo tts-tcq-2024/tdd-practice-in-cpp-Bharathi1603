a570457e01007c39a7ac98ee961cd36e6fff3fd4
Bharathi1603 <bharathikec013@gmail.com>
2024-07-10 13:08:57 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -6,4 +6,9 @@ public:
       int add(const std::string input);
 private:
       void findAndReplaceCharacter(std::string& str, char findCharacter, char replaceCharacter);
+
+      std::vector<int> splitAndFilterNumbers(const std::string& str, char delimiter);
+
+      int performAddition(std::vector<int> numbers);
+
 };
